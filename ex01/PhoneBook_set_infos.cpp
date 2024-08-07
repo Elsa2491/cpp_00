d14552d9990fae357a38c0f45856c5934a255086
elsa2491 <elsa.touma@gmail.com>
2024-08-07 21:59:17 +0200

    [07.08] Print tableau

--- ex01/PhoneBook_set_infos.cpp
+++ ex01/PhoneBook_set_infos.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:50:33 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/06 21:02:04 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/07 20:47:05 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,85 +115,3 @@ std::string	PhoneBook::ft_add_secret()
 		return std::string();
 	return (secret);
 }
-/*
-void	PhoneBook::ft_add_contact(void)
-{
-	Contact	contact;
-	std::string	first_n;
-	std::string	last_n;
-	std::string	nickname;
-	std::string	phone;
-	std::string	secret;
-
-	first_n = ft_add_first_name();
-	if (!first_n.size())
-		return ;
-	last_n = ft_add_last_name();
-	if (!last_n.size())
-		return ;
-	nickname = ft_add_nickname();
-	phone = ft_add_phone();
-	if (!phone.size())
-		return ;
-	secret = ft_add_secret();
-	this->_index += 1;
-	this->_contact[this->_index].ft_set_contact(first_n, last_n, nickname, phone, secret);
-//	this->_index += 1;
-	// if (this->_index == 1)
-	// 	std::cout << "\nYou have " << this->_index << " contact"  << std::endl;
-	// else
-	// 	std::cout << "\nYou have " << this->_index << " contacts"  << std::endl;
-	std::cout << "Enter 'ADD' to save a new one or 'SEARCH' to display a specific one.\n";
-	std::cout << "If you want to quit, please enter 'EXIT'\n";
-	return ;
-}
-
-
-Contact	PhoneBook::ft_retrieve_contact(int i)
-{
-	return  this->_contact[i];
-}
-
-void	PhoneBook::ft_print_instructions(void)
-{
-	while (std::getline(std::cin, this->input)) // && this->_index < 8)
-	{
-		if (!this->input.compare("ADD"))
-			ft_add_contact();
-		else if (!this->input.compare("SEARCH"))
-		{
-			if (!this->_index)
-			{
-				std::cout << "You don't have any contact yet.\n";
-				std::cout << "Please enter 'ADD' to save a new one before searching, or 'EXIT' if you want to quit.\n";
-			}
-			else
-			{
-				std::cout << "You want to " << this->input << std::endl;
-				ft_retrieve_contact(this->_index).ft_print_contact();
-			}
-		}
-		else if (!this->input.compare("EXIT"))
-		{
-			std::cout << "What a pity that you already want to leave us!" << std::endl;
-			return ;
-		}
-		else
-			std::cout << "Please, enter a correct command: ADD, SEARCH or EXIT\n";
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	PhoneBook phonebook;
-
-	(void)argv;
-	if (argc != 1)
-		return (0);
-	std::cout << "Welcome to the 80s and their unbelievable technology!\n\n";
-	std::cout << "Enter 'ADD' to save a new contact or 'SEARCH' to display a specific one.\n";
-	std::cout << "If you want to quit, please enter 'EXIT'\n";
-	phonebook.ft_print_instructions();
-	return (0);
-}
-*/
