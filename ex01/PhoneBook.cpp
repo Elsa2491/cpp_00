d7448a9f7e312b3518731cca0f2f397da3d188e0
elsa2491 <elsa.touma@gmail.com>
2024-08-08 19:37:59 +0200

    [08.08 clean] Add define in .hpp

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,21 +6,12 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:50:33 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/08 18:22:31 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/08 19:24:55 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
-//#include "Contact.hpp"
 
-/*
-   PhoneBook::PhoneBook(void) 
-   {
-   this->_index = 0;
-   }
-
-   PhoneBook::~PhoneBook(void) {}
- */
 void	PhoneBook::ft_add_contact(void)
 {
 	Contact	contact;
@@ -41,13 +32,8 @@ void	PhoneBook::ft_add_contact(void)
 	if (!phone.size())
 		return ;
 	secret = ft_add_secret();
-//	this->_index += 1;
 	this->_contact[this->_index].ft_set_contact(this->_index, first_n, last_n, nickname, phone, secret);
 	this->_index += 1;
-	// if (this->_index == 1)
-	// 	std::cout << "\nYou have " << this->_index << " contact"  << std::endl;
-	// else
-	// 	std::cout << "\nYou have " << this->_index << " contacts"  << std::endl;
 	std::cout << "\nEnter 'ADD' to save a new one or 'SEARCH' to display a specific one.\n";
 	std::cout << "If you want to quit, please enter 'EXIT'\n";
 	return ;
@@ -56,85 +42,56 @@ void	PhoneBook::ft_add_contact(void)
 
 Contact	PhoneBook::ft_retrieve_contact(int i)
 {
-	
+
 	return  (this->_contact[i]);
 }
 
 void	PhoneBook::ft_print_contact(void)
 {
-	const char	*horizontal = "\u2500";
-	const char	*vertical = "\u2502";
-	const char	*corner_tl = "\u250C";
-	const char	*corner_tr = "\u2510";
-	const char	*corner_bl = "\u2514";
-	const char	*corner_br = "\u2518";
-	const char	*cross = "\u253C";
-	const char	*t_top = "\u252C";
-	const char	*t_bottom = "\u2534";
-	const char	*t_left = "\u251C";
-	const char	*t_right = "\u2524";
-
-	std::locale::global(std::locale("en_US.UTF-8"));
-	std::cout << corner_tl;
+	std::cout << CORNER_TL;
 	for (int col = 0; col < 4; ++col)
 	{
 		for (int width = 0; width < 10; width++)
-			std::cout << horizontal;
+			std::cout << HORIZONTAL;
 		if (col < 3)
-			std::cout << t_top;
+			std::cout << T_TOP;
 	}
-	std::cout << corner_tr << std::endl;
+	std::cout << CORNER_TR << std::endl;
 	for (int height = 0; height < this->_index; ++height) {
-		std::cout << vertical;
-//		for (int j = 0; j < 1; j++) {
-			//			std::string cell = data[i][j]; 
-			//std::cout << std::string(11 - cell.length(), ' ') << this->_i << vertical;
-//			ft_retrieve_contact(i); //._i << vertical;
-			//std::cout << std::setw(10) << this->_index << vertical;
-			std::cout << std::setw(10) << height + 1 << vertical;
-			std::cout << std::setw(10) << this->_contact[height].get_first_name() << vertical;
-			std::cout << std::setw(10) << this->_contact[height].get_last_name() << vertical;
-			std::cout << std::setw(10) << this->_contact[height].get_nickname() << vertical;
-			//	std::cout << std::setw(10) << this->_contact[i]._first_name << vertical;
-			//	std::cout << std::setw(10) << this->_contact[i]._last_name << vertical;
-			//	std::cout << std::setw(10) << this->_contact[i]._nickname << vertical;
-			//	std::cout << std::setw(10) << this->_last_name << vertical;
-			//	std::cout << std::setw(10) << this->_nickname << vertical;
-			//			std::cout << std::string(11 - cell.length(), ' ') << this->_first_name << vertical;
-			//			std::cout << std::string(11 - cell.length(), ' ') << this->_last_name << vertical;
-			//			std::cout << std::string(11 - cell.length(), ' ') << this->_nickname << vertical;
-			std::cout << std::endl;
-			if (height < this->_index - 1) {
-				std::cout << t_left;
-				for (int j = 0; j < 4; ++j) {
-					for (int k = 0; k < 10; k++)
-						std::cout << horizontal; // << horizontal << horizontal;
-					if (j < 4 - 1) {
-						std::cout << cross;
-					}
+		std::cout << VERTICAL;
+		std::cout << std::setw(10) << height + 1 << VERTICAL;
+		std::cout << std::setw(10) << this->_contact[height].get_first_name() << VERTICAL;
+		std::cout << std::setw(10) << this->_contact[height].get_last_name() << VERTICAL;
+		std::cout << std::setw(10) << this->_contact[height].get_nickname() << VERTICAL;
+		std::cout << std::endl;
+		if (height < this->_index - 1) {
+			std::cout << T_LEFT;
+			for (int j = 0; j < 4; ++j) {
+				for (int k = 0; k < 10; k++)
+					std::cout << HORIZONTAL;
+				if (j < 4 - 1) {
+					std::cout << CROSS;
 				}
-				std::cout << t_right << std::endl;
 			}
-		//}
+			std::cout << T_RIGHT << std::endl;
+		}
 	}
-	std::cout << corner_bl; // << std::endl;
+	std::cout << CORNER_BL;
 	for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 10; j++)
-			std::cout << horizontal; // << horizontal << horizontal;
+			std::cout << HORIZONTAL;
 		if (i < 3)
-			std::cout << t_bottom;
+			std::cout << T_BOTTOM;
 	}
-	std::cout << corner_br << std::endl;
-	//	}
-	//}
-	//			std::cout << "Enter 'ADD' to save a new one or the index to display a specific one.\n";
-	//			std::cout << "If you want to quit, please enter 'EXIT'\n";
-	}
+	std::cout << CORNER_BR << std::endl;
+	std::cout << "Enter 'ADD' to save a new one or the index to display a specific one.\n";
+	std::cout << "If you want to quit, please enter 'EXIT'\n";
+	// cherche l'index
+}
 
 void	PhoneBook::ft_print_instructions(void)
 {
-	int	i;
 	while (std::getline(std::cin, this->_input))
 	{
 		if (!this->_input.compare("ADD"))
@@ -148,11 +105,7 @@ void	PhoneBook::ft_print_instructions(void)
 				std::cout << "or 'EXIT' if you want to quit.\n";
 			}
 			else
-			{
-				//			for (i = 1; i <= this->_index; i++)
-							//	ft_retrieve_contact(i).ft_print_contact();
 				ft_print_contact();
-			}
 		}
 		else if (!this->_input.compare("EXIT"))
 		{
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,21 +6,32 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:46:16 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/08 18:30:22 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/08 19:27:05 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <cctype>
-#include <iomanip>
-#include "Contact.hpp"
+# include <iostream>
+# include <string>
+# include <cstring>
+# include <cctype>
+# include <iomanip>
+# include "Contact.hpp"
 
-#define NB_CONTACT 8
+# define NB_CONTACT 	8
+# define HORIZONTAL	"\u2500"
+# define VERTICAL	"\u2502"
+# define CORNER_TL	"\u250c"
+# define CORNER_TR	"\u2510"
+# define CORNER_BL	"\u2514"
+# define CORNER_BR	"\u2518"
+# define CROSS		"\u253c"
+# define T_TOP		"\u252c"
+# define T_BOTTOM	"\u2534"
+# define T_LEFT		"\u251c"
+# define T_RIGHT	"\u2524"
 
 int	ft_is_alpha(std::string str);
 int	ft_is_digit(std::string str);
@@ -29,7 +40,7 @@ class	PhoneBook
 {
 	Contact 	_contact[NB_CONTACT];
 	int		_contact_nb;
-	int		_index;
+//	int		_index;
 	std::string	_input;
 	std::string	ft_add_first_name(void);
 	std::string	ft_add_last_name(void);
@@ -45,8 +56,6 @@ public:
 
 	void	ft_print_instructions(void);
 	Contact ft_retrieve_contact(int i);
-//	int		_contact_nb; //static
-//	int		index;
 };
 
 #endif
